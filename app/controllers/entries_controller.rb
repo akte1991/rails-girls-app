47c04ec6bad5b0c50fa3937fbbddd3ffd8a20858
Aleksandra <olajadczyk@gmail.com>
2016-05-22 10:22:59 +0000

    added edit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,9 +5,27 @@ class EntriesController < ApplicationController
     def index
          @entries = Entry.order(date: :asc)
     end
-def create
-  entry_params = params["entry"].permit("title", "contents")
-  entry = Entry.create(entry_params)
-  redirect_to(entry_path(entry))
-end
+    
+    def create
+      entry = Entry.create(entry_params)
+      redirect_to(entry_path(entry))
+    end
+    
+    def edit
+      @entry = Entry.find(params["id"])
+    end
+    
+    def update
+      entry = Entry.create(entry_params)
+      entry = Entry.find(params["id"])
+      entry.update(entry_params)
+      redirect_to(entry_path(entry))
+    end
+
+    private
+    
+    def entry_params
+      params.require(:entry).permit(:title, :contents)
+    end
+    
 end
